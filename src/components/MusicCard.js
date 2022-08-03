eed7b728b41f3de96f7313dfb299837724d98793
Yasmin Reis Tello <mimirt08@gmail.com>
2022-08-03 11:34:17 -0300

    Requisito 07

---
+++ src/components/MusicCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // id: '',
+      // album: [],
+    };
+  }
+
+  render() {
+    const { previewUrl } = this.props;
+    return (
+      <div>
+        <audio
+          data-testid="audio-component"
+          src={ previewUrl }
+          controls
+        >
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+
+};
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,15 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      album: [],
+      isLoading: false,
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const response = await getMusics(id);
+    this.setState({ album: response, isLoading: true });
+    console.log(response);
+  }
+
   render() {
+    const { album, isLoading } = this.state;
     return (
+
       <div data-testid="page-album">
         <Header />
+
+        <div>
+          <p data-testid="artist-name">{isLoading && album[0].artistName}</p>
+          <p data-testid="album-name">{isLoading && album[0].collectionName}</p>
+          {isLoading && album.map((elemento) => (
+            <div key={ elemento.trackId }>
+              <p>{elemento.trackName }</p>
+              {elemento.kind === 'song' && (
+                <MusicCard
+                  previewUrl={ elemento.previewUrl }
+                />)}
+            </div>
+          ))}
+
+        </div>
+
       </div>
 
     );
   }
 }
 
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 export default Album;
